33cd899ac74faff52046ae350f409e9fa2cc1a8b
IllestBrewery4580 <161643583+IllestBrewery4580@users.noreply.github.com>
2025-10-22 21:23:51 -0400

    Fix Logout app: corrected urls and views

--- Backend/ecinema/Logout/urls.py
+++ Backend/ecinema/Logout/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
-<<<<<<< HEAD
 from Logout.views import logout_view  # absolute import since it's a separate app
 
 urlpatterns = [
     path('logout/', logout_view, name='logout'),
-=======
-from .views import logout_view
-
-urlpatterns = [
-    path('accounts/logout', logout_view, name='logout'),
->>>>>>> e9077dd (Added logout button and updated App.js UI)
 ]
--- Backend/ecinema/Logout/views.py
+++ Backend/ecinema/Logout/views.py
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
 from django.contrib.auth import logout
 from django.http import JsonResponse
 
 def logout_view(request):
     logout(request)
     return JsonResponse({'message': 'Logged out seccessfully'})
-=======
-# views.py
-from django.contrib.auth import logout
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-
-@csrf_exempt # if using API calls from React frontend
-def logout_view(request):
-    if request.method == 'GET':
-        logout(request)  # ends the session
-        return JsonResponse({'message': 'Logged out successfully'}, status=200)
-    return JsonResponse({'error': 'Invalid request method'}, status=400)
->>>>>>> e9077dd (Added logout button and updated App.js UI)
