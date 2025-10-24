62eee648d6adc59626f88907a1f643eab04f6a8c
Susan-Awad <152205257+Susan-Awad@users.noreply.github.com>
2025-10-24 19:51:19 -0400

    Add files via upload

---
+++ Backend/ecinema/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('verify-otp/', views.verify_otp, name='verify_otp'),
+    path('login/', views.login_view, name='login'),
+]
