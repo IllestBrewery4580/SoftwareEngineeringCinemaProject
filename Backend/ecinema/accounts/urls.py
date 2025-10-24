00d4b10b0be20e2432e8d2ff542fa01046ccab09
Susan-Awad <152205257+Susan-Awad@users.noreply.github.com>
2025-10-24 19:58:45 -0400

    added changes

--- Backend/ecinema/accounts/urls.py
+++ Backend/ecinema/accounts/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('verify-otp/', views.verify_otp, name='verify_otp'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.get_profile, name='get_profile'),
+    path('csrf/', views.getCSRFToken, name='getCSRFToken'),
+    path('forgotpassword/', views.forgot_password, name='forgot_password'),
+    path('newpassword/', views.new_password, name='new_password'),
+    path('verify-otp-pass/', views.verify_otp_pass, name='verify_otp_pass'),
+    path('changepassword/', views.change_password, name='change_password'),
 ]
