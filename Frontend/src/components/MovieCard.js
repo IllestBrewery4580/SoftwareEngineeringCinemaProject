327bbab56efea88679288c3025c926d426379ca8
Susan-Awad <152205257+Susan-Awad@users.noreply.github.com>
2025-10-25 20:44:10 -0400

    Update MovieCard.js

--- Frontend/src/components/MovieCard.js
+++ Frontend/src/components/MovieCard.js
@@ -2,6 +2,17 @@
 import { Play, Clock } from 'lucide-react';
 
 const MovieCard = ({ movie, showTrailer = false, showMovieDetails, showBookingPage }) => {
+  var rated = null;
+  if(movie.rating == 1) {
+    rated = "G"
+  } else if (movie.rating == 2) {
+    rated = "PG"
+  } else if (movie.rating == 3) {
+    rated = "PG-13"
+  } else {
+    rated = "R"
+  }
+  
   return(
   <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
     <div className="relative">
@@ -11,7 +22,7 @@ const MovieCard = ({ movie, showTrailer = false, showMovieDetails, showBookingPa
         className="w-full h-64 object-cover cursor-pointer"
         onClick={() => showMovieDetails(movie)}
       />
-      {showTrailer && movie.trailer && (
+      {showTrailer && movie.trailer_link && (
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity cursor-pointer"
              onClick={() => showMovieDetails(movie)}>
           <Play className="text-white w-12 h-12" />
@@ -25,7 +36,7 @@ const MovieCard = ({ movie, showTrailer = false, showMovieDetails, showBookingPa
       >
         {movie.title}
       </h3>
-      <p className="text-gray-600 text-sm mb-2">{movie.genre} • ⭐ {movie.rating}</p>
+      <p className="text-gray-600 text-sm mb-2">{rated} • {movie.genre} • ⭐ {movie.review_score}</p>
       <p className="text-gray-700 text-sm mb-3 line-clamp-2">{movie.description}</p>
       
       {/* Showtimes */}
