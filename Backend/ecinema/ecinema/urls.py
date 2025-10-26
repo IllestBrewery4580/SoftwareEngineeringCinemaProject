0a2c6cf9556790bb7900b4a36c144195e69eb270
Susan-Awad <152205257+Susan-Awad@users.noreply.github.com>
2025-10-25 20:50:13 -0400

    Update urls.py

--- Backend/ecinema/ecinema/urls.py
+++ Backend/ecinema/ecinema/urls.py
@@ -24,7 +24,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from movie.views import MovieViewSet
+from movie.views import MovieViewSet, MovieShowViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'movies', MovieViewSet)
