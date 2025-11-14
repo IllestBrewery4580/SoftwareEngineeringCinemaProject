0d148931950b7c28732acefee71654f111a047de
IllestBrewery4580 <161643583+IllestBrewery4580@users.noreply.github.com>
2025-11-14 16:11:32 -0500

    Update admin.py

--- Backend/ecinema/promotions/admin.py
+++ Backend/ecinema/promotions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Promotion
 
-# Register your models here.
+@admin.register(Promotion)
+class PromotionAdmin(admin.ModelAdmin):
+    list_display = ("code", "discount_percent", "start_daate", "end_date", "is_valid")
+    search_fields = ("code",)
+    list_filter = ("start_date", "end_date")
