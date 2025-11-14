28c1fa0436b6f5ce8ff2fbd35b63b71c8dcb05a2
IllestBrewery4580 <161643583+IllestBrewery4580@users.noreply.github.com>
2025-11-14 15:05:22 -0500

    Add promotions frontend and related backend changes

---
+++ src/pages/Booking.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Booking() {
+    return (
+        <div>
+            <h1>Booking Page</h1>
+            <p>Prototype UI: show movie title and showtime here.</p>
+        </div>
+    );
+}
+
+export default Booking;
