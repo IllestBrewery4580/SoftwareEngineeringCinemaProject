675d6b2ac5ecd3810d412049fa6b98af1060e8e3
IllestBrewery4580 <161643583+IllestBrewery4580@users.noreply.github.com>
2025-11-14 16:12:40 -0500

    Update models.py

--- Backend/ecinema/promotions/models.py
+++ Backend/ecinema/promotions/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Promotion(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True)
+    discount_percent = models.PositiveIntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def is_valid(self):
+        today = timezone.now().date()
+        
+        if not self.ative:
+            return False
+        
+        if self.start_date and self.start_date > today:
+            return False
+        
+        if self.end_date and self.end_date < today:
+            return False
+        
+        return True
+    
+    def __str__(self):
+        return self.code
